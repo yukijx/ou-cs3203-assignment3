c60313054a9a689e1793c50223d0ce82898bec3b
yukijx <yukizhengjx@gmail.com>
2023-02-21 00:08:03 -0600

    added main function

--- ou-cs3203-assignment3.cpp
+++ ou-cs3203-assignment3.cpp
@@ -9,6 +9,11 @@ class assignment3{
 private: 
     vector<int> vect; 
 public: 
+
+     void add_number(int number) { 
+        vect.push_back(number);
+    };
+
     int add(){
          int sum = 0; 
 
@@ -28,3 +33,18 @@ public:
     }; 
 
 }; 
+
+int main(){ 
+
+        assignment3 vector; 
+        cout<< "Hello! Please input integers to create a vector! Put in a letter to indicate you're finished."<<endl; 
+
+        int current, count; 
+        while(cin >> current){ 
+            ++count; 
+            vector.add_number(current);
+        }
+
+        cout << "The total sum of all values is " << vector.add() << "." << endl;
+        cout << "The product of all the values is " << vector.product() << "." << endl; 
+} 
